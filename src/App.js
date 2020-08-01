10ec0635cb52d5edf44930da893a894e9a613bea
RafaelFreimuth <rafaelfreimuth@outlook.com>
2020-07-31 23:32:14 -0300

    criando carrousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import Menu from './components/Menu'
+import Menu from './components/Menu';
+import Footer from './components/Footer';
+import BannerMain from './components/BannerMain'
+import dadosIniciais from './data/dados_iniciais.json';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
     <div>
       <Menu />
+      <BannerMain 
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+      />
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[1]}
+      />
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[2]}
+      />
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[3]}
+      />
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[4]}
+      />
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[5]}
+      />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -4,10 +4,9 @@ import styled from 'styled-components';
 export const FooterBase = styled.footer`
   background: var(--black);
   border-top: 2px solid var(--primary);
-  padding-left: 16px;
-  padding-right: 16px;
   padding-top: 32px;
   padding-bottom: 32px;
+  width: 100%;;
   color: var(--white);
   text-align: center;
   @media (max-width: 800px) {
